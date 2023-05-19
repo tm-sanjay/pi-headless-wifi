5250c0598b357c3433d9a02a6f75e931469db32b
tm-sanjay <tmsanjay.in@gmail.com>
2023-05-19 20:58:48 +0530

    Connect to the specified WiFi from portal

--- main.go
+++ main.go
@@ -13,15 +13,19 @@ type WifiDetails struct {
 	Name string `json:"name"`
 }
 
+const (
+	port = "8080" // Port to run the server on
+)
+
 func main() {
 	fmt.Println("Starting server...")
-	fmt.Println("localhost:8080")
+	fmt.Println("localhost:" + port)
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/submit", submit)
 	http.HandleFunc("/wifilist", getWifiList)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 // Function to handle the home page
@@ -40,8 +44,12 @@ func submit(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SSID:", wifiSSID)
 	fmt.Println("Password:", wifiPSK)
 
+	// Connect to the specified WiFi network
+	connectToWifi(wifiSSID, wifiPSK)
+
 	// Redirect the user back to the home page
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
 
 // Function to get the list of available WiFi networks and return them as JSON
@@ -49,26 +57,45 @@ func getWifiList(w http.ResponseWriter, r *http.Request) {
 	// Execute the nmcli command to get the list of WiFi networks
 	out, err := exec.Command("nmcli", "-f", "SSID", "dev", "wifi", "list").Output()
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	// Parse the output and create a slice of Wifi objects
-	log.Println("-----Available WiFi Networks-----")
+	// log.Println("-----Available WiFi Networks-----")
 	wifiList := make([]WifiDetails, 0)
 	lines := strings.Split(string(out), "\n")
 	for i := 1; i < len(lines); i++ {
 		line := strings.TrimSpace(lines[i])
-		if line != "" {
+		if line != "" && line != "--" {
 			wifiList = append(wifiList, WifiDetails{Name: line})
-			log.Println(line)
+			// log.Println(line)
 		}
 	}
 
 	// Marshal the slice to JSON and write to the response
 	wifiListJson, err := json.Marshal(wifiList)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(wifiListJson)
+}
+
+// Function to connect to a WiFi network
+func connectToWifi(wifiSSID string, wifiPSK string) {
+	// Execute the nmcli command to connect to the specified WiFi network
+	out, err := exec.Command("nmcli", "dev", "wifi", "connect", wifiSSID, "password", wifiPSK).Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Print the output to the console to confirm the connection was successful
+	fmt.Println(string(out))
+
+	//if connection was successful
+	if strings.Contains(string(out), "successfully activated") {
+		log.Println("Successfully connected to", wifiSSID)
+	} else {
+		log.Println("!Failed to connect to", wifiSSID)
+	}
 }
